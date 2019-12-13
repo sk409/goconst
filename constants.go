d04142ccbc01050586a070015459a6ae3a27f186
Shoutokobayashi <deepstrap353@gmail.com>
2019-12-13 09:38:46 +0900

    first commit

---
+++ constants.go
@@ -0,0 +1,16 @@
+package goconst
+
+const (
+	HTTP_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials"
+	HTTP_HEADER_ACCESS_CONTROL_ALLOW_HEADERS     = "Access-Control-Allow-Headers"
+	HTTP_HEADER_ACCESS_CONTROL_ALLOW_METHODS     = "Access-Control-Allow-Methods"
+	HTTP_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN      = "Access-Control-Allow-Origin"
+	HTTP_HEADER_CONTENT_ENCODING                 = "Content-Encoding"
+	HTTP_HEADER_CONTENT_ENCODING_GZIP            = "gzip"
+	HTTP_HEADER_CONTENT_TYPE                     = "Content-Type"
+	HTTP_HEADER_CONTENT_TYPE_JSON                = "application/json"
+	HTTP_HEADER_CONTENT_TYPE_URLENCODED          = "application/x-www-form-urlencoded"
+	HTTP_HEADER_X_XSRF_TOKEN                     = "X-XSRF-Token"
+	TYPE_INT                                     = "int"
+	TYPE_STRING                                  = "string"
+)
